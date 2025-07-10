32a8b5f3b7337e1475a577a9094dcaa9727c597b
Eduardo Alvarez <yalvarezg82@gmail.com>
2025-07-09 22:17:51 -0500

    actulizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "https://prueba-crud-laravel.onrender.com/api/usuarios";
+const API_URL = "https://prueba-crud-laravel.onrender.com";
 
 export default function App() {
   const [usuarios, setUsuarios] = useState([]);
